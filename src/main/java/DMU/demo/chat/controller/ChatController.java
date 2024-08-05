c66549e65740d4ea47e636422cbf8d92967fa91e
Seungwon Lee <seungwon.dev@gmail.com>
2024-08-05 18:56:15 +0900

    config: CORS 설정

--- src/main/java/DMU/demo/chat/controller/ChatController.java
+++ src/main/java/DMU/demo/chat/controller/ChatController.java
@@ -4,12 +4,14 @@ import DMU.demo.chat.domain.entity.ChatMessage;
 import DMU.demo.chat.domain.repository.ChatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
