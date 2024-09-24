c0081cd788a63ad34b8dff204aa48ef591775dcc
Seungwon Lee <seungwon.dev@gmail.com>
2024-09-24 21:05:22 +0900

    feat: 분실물 API 마이그레이션
    FastAPI 서버에서 "/lostfound" 엔드포인트 마이그레이션

---
+++ src/main/java/DMU/demo/lostfound/domain/entity/LostFound.java
@@ -0,0 +1,26 @@
+package DMU.demo.lostfound.domain.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Document(collection = "lostfound")
+public class LostFound {
+    @Id
+    private String _id;
+    private String atcId;
+    private String clrNm;
+    private String depPlace;
+    private String fdFilePathImg;
+    private String fdPrdtNm;
+    private String fdSbjt;
+    private String fdSn;
+    private String fdYmd;
+    private String prdtClNm;
+    private String rnum;
+    private LocalDateTime updatedAt;
+}
+
