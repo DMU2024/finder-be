dcdb218c7f90094ae9b35c55b62bba176cbe5ef0
Seungwon Lee <seungwon.dev@gmail.com>
2024-09-26 11:00:07 +0900

    feat: 분실물 API 마이그레이션
    FastAPI 서버에서 "/mock" 엔드포인트 마이그레이션

--- src/main/java/DMU/demo/board/domain/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package DMU.demo.board.domain.repository;
-
-import DMU.demo.board.domain.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Integer>{
-}
